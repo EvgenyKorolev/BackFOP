8343502c1d657439585f7997008f233838b64886
Evgeny Korolev <79297269463@yandex.ru>
2020-09-13 13:35:14 +0300

    Update requestping.cpp

--- src/requestping.cpp
+++ src/requestping.cpp
@@ -12,6 +12,7 @@ void RequestPing::handleRequest(Poco::Net::HTTPServerRequest &requestServer, Poc
 {
     (void)requestServer;
     responce.setStatus(Poco::Net::HTTPResponse::HTTP_FOUND);
+    responce.setStatus(Poco::Net::HTTPResponse::HTTP_FOUND);
     QByteArray ret("pong");
     responce.sendBuffer(ret.data(), ret.size());
 }
